bd8a73c7ece7cd0d63e15b65dfcfc3fb4fdf6566
mieumje <66072832+mieumje@users.noreply.github.com>
2020-11-08 02:25:51 +0900

    #4.5 Meta Class and finished Room

--- rooms/models.py
+++ rooms/models.py
@@ -21,28 +21,33 @@ class RoomType(AbstractItem):
 
     """ doctype RoomType Model definition """
 
-    pass
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name"]
 
 
 class Amenity(AbstractItem):
 
     """ Amenity Model definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """ Facility Model definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
 
     """ HouseRule Model definition """
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
 
 
 class Room(core_models.TimeStampedModel):
@@ -64,9 +69,9 @@ class Room(core_models.TimeStampedModel):
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
     room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    HouseRule = models.ManyToManyField(HouseRule)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    HouseRule = models.ManyToManyField(HouseRule, blank=True)
 
     def __str__(self):
         return self.name
