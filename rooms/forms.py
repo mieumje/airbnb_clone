7b95881d4c1d4cc43dcc0a97717b852c0b91078b
mieumje <66072832+mieumje@users.noreply.github.com>
2020-11-15 20:28:09 +0900

    #13.10 Finishing Up

--- rooms/forms.py
+++ rooms/forms.py
@@ -18,8 +18,12 @@ class SearchForm(forms.Form):
     instant_book = forms.BooleanField(required=False)
     superhost = forms.BooleanField(required=False)
     amenities = forms.ModelMultipleChoiceField(
-        queryset=models.Amenity.objects, widget=forms.CheckboxSelectMultiple
+        required=False,
+        queryset=models.Amenity.objects,
+        widget=forms.CheckboxSelectMultiple,
     )
     facilities = forms.ModelMultipleChoiceField(
-        queryset=models.Facility.objects, widget=forms.CheckboxSelectMultiple
+        required=False,
+        queryset=models.Facility.objects,
+        widget=forms.CheckboxSelectMultiple,
     )
