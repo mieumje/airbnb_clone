b37e6a8122f309b484d5eea553fc3f7175da9c98
mieumje <66072832+mieumje@users.noreply.github.com>
2020-11-24 23:34:54 +0900

    #24.1 calendar

--- cal.py
+++ cal.py
@@ -1,25 +1,34 @@
 import calendar
 
 
-class Calendar:
+class Calendar(calendar.Calendar):
     def __init__(self, year, month):
+        super().__init__(firstweekday=0)
         self.year = year
         self.month = month
-        self.days = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+        self.days_n = ("월", "화", "수", "목", "금", "토", "일")
         self.months = (
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
+            "1 월",
+            "2 월",
+            "3 월",
+            "4 월",
+            "5 월",
+            "6 월",
+            "7 월",
+            "8 월",
+            "9 월",
+            "10 월",
+            "11 월",
+            "12 월",
         )
+
+    def get_days(self):
+        weeks = self.monthdays2calendar(self.year, self.month)
+        days = []
+        for week in weeks:
+            for day, _ in week:
+                days.append(day)
+        return days
 
     def get_month(self):
         return self.months[self.month - 1]
--- rooms/models.py
+++ rooms/models.py
@@ -119,6 +119,6 @@ class Room(core_models.TimeStampedModel):
         return photos
 
     def get_calendars(self):
-        calendar = Calendar(2020, 12)
-        print(calendar.get_month())
-        return False
+        this_month = Calendar(2020, 12)
+        next_month = Calendar(2021, 1)
+        return (this_month, next_month)
